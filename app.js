63b8114b6e37c790d9cc838ac50c095c417008ab
Mary Robin Rose <maryrobinrose@gmail.com>
2019-05-01 17:33:36 -0700

    remove comments

--- app.js
+++ app.js
@@ -31,9 +31,6 @@ app.use('/project/:id', (req, res) => {
   res.render('project', { project });
 });
 
-//App uses a static route to serve the static files in the public folder
-
-
 //Error handler for non-existant route
 app.use((req, res, next) => {
   const err = new Error('Not Found');
